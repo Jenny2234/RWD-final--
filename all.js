8861f9356a6c5e68346ee130b5bbc55c2d44b665
Jenny2234 <98068500+Jenny2234@users.noreply.github.com>
2022-09-04 14:08:25 +0800

    layout調整

--- all.js
+++ all.js
@@ -3,4 +3,19 @@ $(document).ready(function(){
       e.preventDefault();
       $('body').toggleClass('menu-show');
     });
+    $(".scrollTop").click(function (event) {
+      event.preventDefault();
+      var target = $(this).attr("href");
+      var targetPosition = $(target).offset().top;
+      $("html,body").animate({ scrollTop: targetPosition }, 1000);
+    });
+    $(".top a").click(function (event) {
+      event.preventDefault();
+      $("html,body").animate(
+        {
+          scrollTop: 0
+        },
+        1000
+      );
+    });
   });
